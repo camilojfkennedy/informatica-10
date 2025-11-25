c1e64f20f0cc476d4b5dca3644254dbca49e1c67
Camilo Fonseca <camilo.fonseca@sala115.tail6a1051.ts.net>
2025-11-25 16:43:53 -0500

    se trabajaron las teorías

---
+++ docs/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'informatica-10'
+copyright = '2025, Julian Camilo Fonseca Romero, IE John F. Kennedy'
+author = 'Julian Camilo Fonseca Romero'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser","sphinx_design"]
+
+myst_enable_extensions = [
+    #"amsmath",
+    "dollarmath", #ecuaciones matematicas con $
+    "tasklist", # listas con casillas de verificacion pero como hacer para que sea editable?
+    "colon_fence", #bloques con ::: en vez de ```
+    "attrs_inline", # para agregar atributos en linea ejemplo: IMAGENES
+    "smartquotes", #comillas inteligentes
+    #"linkify", # convierte URLs en enlaces automaticamente
+    "fieldlist" # listas de campos
+]
+
+myst_number_code_blocks = ["python", "c++"] # Numera bloques de código en los lenguajes especificados
+myst_links_external_new_tab = True # Abre enlaces externos en una nueva pestaña
+myst_enable_checkboxes = True # Habilita casillas de verificación en las listas de tareas
+myst_heading_anchors = 3 # Genera enlaces ancla para los encabezados (niveles 1 a 3)
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'es'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.md
@@ -0,0 +1,62 @@
+:::{toctree}
+:hidden:
+:maxdepth: 2
+:caption: CONTENIDO
+
+intro.md
+:::
+
+:::{toctree}
+:hidden:
+:maxdepth: 2
+:caption: PERIODO 01
+
+periodo01/A01.md
+periodo01/R01.md
+:::
+
+:::{toctree}
+:hidden:
+:maxdepth: 2
+:caption: PERIODO 02
+
+periodo02/R02.md
+:::
+
+:::{toctree}
+:hidden:
+:maxdepth: 2
+:caption: PERIODO 03
+
+periodo03/R03.md
+:::
+
+:::{toctree}
+:hidden:
+:maxdepth: 2
+:caption: PERIODO 04
+
+
+periodo04/R04.md
+:::
+
+:::{toctree}
+:hidden:
+:maxdepth: 2
+:caption: RECURSOS
+
+recursos/T01.md
+recursos/T02.md
+recursos/T03.md
+recursos/T04.md
+recursos/T05.md
+recursos/T06.md
+recursos/curriculum.md
+recursos/referencias.md
+:::
+
+# Informática grado décimo
+
+En este portal encuentra las guías y recursos académicos necesarios para aprender sobre las siguientes temáticas:
+
+- 
